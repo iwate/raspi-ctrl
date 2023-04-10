f4072085544979af8ddeed78202d345c4c33187f
iwate <github@iwate.me>
2023-04-10 17:25:52 +0900

    add setup scripts

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+wget https://github.com/iwate/raspi-ctrl/releases/latest/download/raspictrl
+
+wget https://github.com/iwate/raspi-ctrl/raw/master/raspictrl.service
+
+sudo cp ./raspictrl.service /etc/systemd/system/raspictrl.service
+
+sudo systemctl enable mydaemon.service
+
+sudo swapoff --all
+sudo systemctl stop dphys-swapfile
+sudo systemctl disable dphys-swapfile
+
+sudo echo 'tmpfs	/tmp	tmpfs	defaults,size=64m,noatime,mode=1777	0	0' >> /etc/fstab
+sudo echo 'tmpfs	/var/tmp	tmpfs	defaults,size=16m,noatime,mode=1777	0	0' >> /etc/fstab
+sudo echo 'tmpfs	/var/log	tmpfs	defaults,size=32m,noatime,mode=0755	0	0' >> /etc/fstab
+
+sudo rm -rf /tmp
+sudo rm -rf /var/tmp
+sudo rm -rf /var/log
+
+sudo reboot
